e0ccec93e4a4443fcbfca537328810462011aef5
Nora El-Zein <nora.ez11@gmail.com>
2025-02-25 15:01:36 +0000

    preprocessing and filtering finished

--- preprocessing.sh
+++ preprocessing.sh
@@ -12,6 +12,5 @@ for file in *.csv.bz2; do
     #Use bzcat to read the .bz2 file, then use cut to remove specified columns, and save the output
     bzcat "$file" | cut -d',' --complement -f3,4,5,6,7,8,11,12,13,14,15,20,21,23,24,25,26,27,28,29 > "$output_file"
 
-    #Remove the original decompressed file if you don't want to keep it
     # rm "${file%.bz2}"
 done
